9930742827f5da98044166b61728706c8d3dbddf
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2024-12-01 19:50:25 -0300

    Aula 8

---
+++ src/main/java/com/devsouzx/cache/agendador/CacheAgendado.java
@@ -0,0 +1,19 @@
+package com.devsouzx.cache.agendador;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Log4j2
+public class CacheAgendado {
+    @Scheduled(fixedDelay = 60, timeUnit = TimeUnit.SECONDS)
+    @CacheEvict("empresas")
+    public void limparCacheEmpresas() {
+        log.info("Executado: " + LocalDateTime.now());
+    }
+}
