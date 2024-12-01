9d2381bb543455ec1597e6489faf6f06785dbc76
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2024-12-01 19:13:48 -0300

    Aula 6

--- src/main/java/com/devsouzx/cache/services/EmpresaService.java
+++ src/main/java/com/devsouzx/cache/services/EmpresaService.java
@@ -3,6 +3,7 @@ package com.devsouzx.cache.services;
 import com.devsouzx.cache.entity.Empresa;
 import com.devsouzx.cache.repository.EmpresaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class EmpresaService {
     @Autowired
     private EmpresaRepository empresaRepository;
 
+    @Cacheable("empresas")
     public List<Empresa> findAll() {
         return (List<Empresa>) empresaRepository.findAll();
     }
